0f4551a5f80a3063ef52d078cab5692ff70b51e1
Forg0ttenUser <lucasdasghagasdesouza@gmail.com>
2025-06-12 20:09:22 -0300

    Aprimora tb-verdade

--- tabela-verdade.py
+++ tabela-verdade.py
@@ -5,30 +5,41 @@ posicao_valor = 1
 base = 2
 expoente = 1
 
-
 #Variável de entrada:
-num_proposicoes = int(input("""Insira o número de proposições
-(Lembre-se: o número pertence ao conjunto dos números naturais e é maior ou igual a 1): """))
+try:
+    num_proposicoes = int(input("""Insira o número de proposições
+    (Lembre-se: o número pertence ao conjunto dos números naturais e é maior ou igual a 1): """))
+except ValueError:
+    print(f"\nErro: entrada não é um número natural. Por favor, redigite o número baseado nas condições de inserção.")
 
-#Criação de lista de listas: 
-for _ in range(num_proposicoes):
-    tabela_verdade.append([])
+else:
 
-#Geração da tabela-verdade:
-for i in range(num_proposicoes):
-    for j in range(base**expoente):
-        for k in range(int(base**num_proposicoes/(base**expoente))):
-            if (posicao_valor == 1):
-                tabela_verdade[i].append(valor_logico[posicao_valor])
-                if (k == (base**num_proposicoes/(base**expoente)) - 1):
-                    posicao_valor = 0
+    #Criação de lista de listas: 
+    for index_proposicional in range(num_proposicoes):
+        tabela_verdade.append([f"p{index_proposicional+1}"])
+
+    #Geração da tabela-verdade:
+    for i in range(num_proposicoes):
+        for j in range(base**expoente):
+            for k in range(int(base**num_proposicoes/(base**expoente))):
+                if (posicao_valor == 1):
+                    tabela_verdade[i].append(valor_logico[posicao_valor])
+                    if (k == (base**num_proposicoes/(base**expoente)) - 1):
+                        posicao_valor = 0
+                else:
+                    tabela_verdade[i].append(valor_logico[posicao_valor])
+                    if (k == (base**num_proposicoes/(base**expoente)) - 1):
+                        posicao_valor = 1
+        expoente+=1
+
+    #Exibição da tabela-verdade de n proposições:
+    print("\n"+"-------"*num_proposicoes)
+    for indice in range(base**num_proposicoes+1):
+        for lista in tabela_verdade:
+            if indice == 0:
+                print(f'|  {lista[indice]} |', end="")
             else:
-                tabela_verdade[i].append(valor_logico[posicao_valor])
-                if (k == (base**num_proposicoes/(base**expoente)) - 1):
-                    posicao_valor = 1
-    expoente+=1
-#Exibição da tabela-verdade de n proposições:
-for n in range(base**num_proposicoes):
-    for e in tabela_verdade:
-        print(f'| {e[n]} |', end="")
-    print()
+                print(f'|  {lista[indice]}  |', end="")
+        print()
+        print("-------"*num_proposicoes, end="")    
+        print()
