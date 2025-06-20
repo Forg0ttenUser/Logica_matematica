e71239e6217fec05d0492fdb7350d6d7e281c8d7
Forg0ttenUser <lucasdasghagasdesouza@gmail.com>
2025-06-20 20:06:52 -0300

    Troca Lista por Dict

--- tabela-verdade.py
+++ tabela-verdade.py
@@ -12,7 +12,7 @@ else:
     else:
         #Variavéis globais:
         valores_verdade = ('F', 'V')
-        tabela_verdade = []
+        tabela_verdade = {}
         valor_posicional_da_tupla = 1
         base = 2
         expoente = 1
@@ -22,7 +22,7 @@ else:
 
         #Criação da matriz tabela-verdade: 
         for indice_proposicional in range(num_proposicoes):
-            tabela_verdade.append([f"p{indice_proposicional+1}"])
+            tabela_verdade[f"p{indice_proposicional+1}"] = []
 
     
         #Preenchimento da matriz tabela-verdade:
@@ -30,11 +30,11 @@ else:
             for j in range(potencia_de_2):
                 for k in range(int(num_linhas/potencia_de_2)):
                     if (valor_posicional_da_tupla == 1):
-                        tabela_verdade[i].append(valores_verdade[valor_posicional_da_tupla])
+                        tabela_verdade[f'p{i+1}'].append(valores_verdade[valor_posicional_da_tupla])
                         if (k == (num_linhas/potencia_de_2) - 1):
                             valor_posicional_da_tupla = 0
                     else:
-                        tabela_verdade[i].append(valores_verdade[valor_posicional_da_tupla])
+                        tabela_verdade[f'p{i+1}'].append(valores_verdade[valor_posicional_da_tupla])
                         if (k == (num_linhas/potencia_de_2) - 1):
                             valor_posicional_da_tupla = 1
             expoente+=1; potencia_de_2 = base**expoente
@@ -42,12 +42,12 @@ else:
 
         #Exibição da matriz tabela-verdade de n proposições:
         print("\n"+"-------"*num_proposicoes)
-        for index in range(num_linhas+1):
-            for lista in tabela_verdade:
-                if index == 0:
-                    print(f'|  {lista[index]} |', end="")
-                else:
-                    print(f'|  {lista[index]}  |', end="")
+        for chave in tabela_verdade.keys():
+            print(f'|  {chave} |', end="")
+        print("\n"+"-------"*num_proposicoes)
+        for index in range(num_linhas):
+            for valor in tabela_verdade.values():
+                    print(f'|  {valor[index]}  |', end="")
             print()
             print("-------"*num_proposicoes, end="")    
             print()
