6cff7ad08905e18fd9e58e1e9ead5d92f120e417
Forg0ttenUser <lucasdasghagasdesouza@gmail.com>
2025-09-06 08:50:44 -0300

    Re-estrutura o sistema web

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template
-from tb import tabela_verdade, num_linhas
+from flask import Flask, render_template, request, redirect, url_for
+from tb import gerar_tabela
 
 app = Flask(__name__)
 
-cont = 1
+
+
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html", var = tabela_verdade,enum = enumerate, linha = num_linhas)
+def pagina_apresentacao():
+    return  render_template("apresentacao.html")
+
+@app.route('/1', methods=['GET','POST'])
+def pagina_tabela():
+    controlador_exibicional = False
+    if request.method == 'POST':
+        retorno_funcao_gerar = gerar_tabela(int(request.form.get("tabela")))
+        controlador_exibicional = True
+        return render_template("index.html", tabela = retorno_funcao_gerar[0], num_linhas = retorno_funcao_gerar[1], exibicao_tabela = controlador_exibicional)
+    return render_template("index.html", exibicao_tabela = controlador_exibicional)
 
 
 if __name__ == '__main__':
