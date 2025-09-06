a34a62306a736b22190dfb109dd27185d482ddcf
Forg0ttenUser <lucasdasghagasdesouza@gmail.com>
2025-09-06 12:23:05 -0300

    Adiciona estilização e corrige ortografia

--- app.py
+++ app.py
@@ -4,20 +4,27 @@ from tb import gerar_tabela
 app = Flask(__name__)
 
 
-
-
 @app.route('/')
 def pagina_apresentacao():
     return  render_template("apresentacao.html")
 
-@app.route('/1', methods=['GET','POST'])
+@app.route('/fundamentacao')
+def pagina_fundamentacao():
+    return  render_template("fundamentacao.html")
+
+
+@app.route('/tabela', methods=['GET','POST'])
 def pagina_tabela():
     controlador_exibicional = False
+    mensagem_erro = False
     if request.method == 'POST':
-        retorno_funcao_gerar = gerar_tabela(int(request.form.get("tabela")))
-        controlador_exibicional = True
-        return render_template("index.html", tabela = retorno_funcao_gerar[0], num_linhas = retorno_funcao_gerar[1], exibicao_tabela = controlador_exibicional)
-    return render_template("index.html", exibicao_tabela = controlador_exibicional)
+        if  request.form.get("tabela").isnumeric():
+            retorno_funcao_gerar = gerar_tabela(int(request.form.get("tabela")))
+            controlador_exibicional  = True
+            return render_template("gerador.html", tabela = retorno_funcao_gerar[0], num_linhas = retorno_funcao_gerar[1], exibicao_tabela = controlador_exibicional, erro = mensagem_erro)
+        mensagem_erro = True
+        return render_template("gerador.html", exibicao_tabela = controlador_exibicional, erro = mensagem_erro)
+    return render_template("gerador.html", exibicao_tabela = controlador_exibicional, erro = mensagem_erro)
 
 
 if __name__ == '__main__':
